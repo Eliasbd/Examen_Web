d1edd87abca327aa57338743107e1d07f1bf70d6
Niall2612 <97602716murcia@gmail.com>
2018-09-13 04:09:59 -0600

    formulario login

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" >
+    <title>Login</title>
+</head>
+<body class="bg-info p-5">
+<div class="container  w-50 pt-5" >
+        <form class="p-4  bg-light rounded" >
+        <div class="text-center h2">
+            <caption>Login</caption>
+      </div>
+<form>
+  
+  <div class="form-group">
+    <label for="exampleInputPassword1">Correo Electronico</label>
+    <input type="email" class="form-control" name="l-correo" id="correo" placeholder="Ingrese su Correo Electronico">
+  </div>
+  
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input type="password" class="form-control" name="l-contraseña" id="contraseña" placeholder="Ingrese su Contraseña">
+  </div>
+  
+  <button type="submit" class="btn btn-primary w-100 ">Iniciar Sesion</button>
+</form>
+</body>
+</html>
