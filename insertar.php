29e8b3f0b844242f8cd3f7431b3b732c989d8ed3
inapro1913 <inapro1913@gmail.com>
2018-09-20 12:12:49 -0600

    esquivar usuarios repetidos

--- insertar.php
+++ insertar.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" >
-    <title>Document</title>
-</head>
-<body>
 <?php
 include 'conexion.php';
 
@@ -17,17 +7,32 @@ $nombre=$_POST['nombre'];
 $correo=$_POST['correo'];
 $contraseña=$_POST['contraseña'];
 
+$verificar_id=mysqli_QUERY($conexion,"SELECT*FROM teacher WHERE id='$id'");
+if(mysqli_num_rows($verificar_id)>0){
+    echo'<script>
+    alert ("ID YA EXISTE");
+    </script>';
+  echo'<meta http-equiv="refresh"content="5;URL=./index.php">';
+    exit;
+    
+}
+
+$verificar_correo=mysqli_QUERY($conexion,"SELECT*FROM teacher WHERE correo='$correo'");
+if(mysqli_num_rows($verificar_correo)>0){
+    echo'<script>
+    alert ("CORREO YA EXISTE");
+    </script>';
+    echo'<meta http-equiv="refresh"content="5;URL=./index.php">';
+   exit;
+    
+}
+
 $insertar = "INSERT  INTO teacher (id, nombre,correo,contra)VALUES('$id','$nombre','$correo','$contraseña')";
 $res = mysqli_query($conexion , $insertar);
 if(!$res){
     echo'Usuario no se pudo ingresar';
 }else{
-    echo'<div class="mensaje text-light bg-dark text-center">
-   <h1 class="bg-primary"> Usuario registrado</h1>
-   <a class="btn btn-primary" href="login.php">Iniciar Sesion</a>
-   </div>';
+    echo'Usuerio registrado';
 }
+
 ?>
-    
-</body>
-</html>
