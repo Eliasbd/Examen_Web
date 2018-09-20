c1147ba6f48cd5e51420f693a2a9af3c8a9a937e
inapro1913 <inapro1913@gmail.com>
2018-09-20 09:35:16 -0600

    aqui esta el archivo de la conexion de php

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "bdregistros");
+
+if(!$conexion){
+    echo'Error al conectar con la base de datos';
+}else{
+    echo'Se conecto exitosamente';
+}
+?>
